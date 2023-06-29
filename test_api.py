9c63444d43f690b7b2a7cf2f1d5194eb998871c5
batamsieuhang <20021460@vnu.edu.vn>
2023-06-30 00:21:16 +0700

    function get data

--- test_api.py
+++ test_api.py
@@ -1,21 +1,26 @@
 import requests
 import json
 
-url = "https://api.livecoinwatch.com/coins/list"
+def get_data():
+    url = "https://http-api.livecoinwatch.com/markets"
+    payload = {
+        "exchange": "binance",
+        "sort": "depth",
+        "order": "descending",
+        "offset": "0",
+        "limit": "30",
+        "search": "usdt"
+    }
 
-payload = json.dumps({
-    "currency": "USD",
-    "sort": "rank",
-    "order": "ascending",
-    "offset": 0,
-    "limit": 100,
-    "meta": False
-})
-headers = {
-    'content-type': 'application/json',
-    'x-api-key': '58c9be28-962f-4177-b6a0-dec1f6968f7d'
-}
+    headers = {
+        'content-type': 'application/json',
+        'User-Agent': 'Mozilla/5.0',
+        'x-api-key': '58c9be28-962f-4177-b6a0-dec1f6968f7d'
+    }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.get(url, headers=headers, params=payload)
 
-print(response.text)
+
+    print(json.loads(response.text)["data"][0])
+
+get_data()
