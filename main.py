ff84cc27af0843bcf39a0b5588298dedd4ab0be4
batamsieuhang <20021460@vnu.edu.vn>
2023-07-04 17:22:46 +0700

    add exception

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from api.call_api import get_data
 from api.convert_api import convert_api
 from save_data.db_save_all import save_all_data
-import time
+import time,json
 
 
 while(True):
     for i in range(1,11):
         response,request_time = get_data(i)
-        data, coin_name = convert_api(response)
-        save_all_data(data,coin_name,request_time)
+        print(response)
+        try:
+            data, coin_name = convert_api(response)
+        except json.decoder.JSONDecodeError as err:
+            continue
         print(request_time)
     time.sleep(5)
