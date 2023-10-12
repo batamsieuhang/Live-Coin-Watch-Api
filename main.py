cfa869c50b4fd7154849b5dc2bd08852b10c62c9
batam-linux-mac <20021460@vnu.edu.vn>
2023-10-12 15:10:03 +0700

    convert unix time to timestamp

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from api.call_api import get_data
 from api.convert_api import convert_api
 from save_data.db_save_all import save_all_data
-import time,json
+import time,json,datetime
 
 
 while(True):
@@ -12,6 +12,6 @@ while(True):
         save_all_data(data,coin_name,request_time)
     except json.decoder.JSONDecodeError as err:
         continue
-    print(request_time)
+    print(datetime.datetime.fromtimestamp(request_time))
     print("Waiting for 15s.....")
     time.sleep(15)
