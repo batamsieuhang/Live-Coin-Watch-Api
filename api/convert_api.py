3e1fc0177c696bc777e88da04375512098f9a6f8
batam-linux-mac <20021460@vnu.edu.vn>
2023-10-12 15:09:25 +0700

    remove print detail coin

--- api/convert_api.py
+++ api/convert_api.py
@@ -8,5 +8,4 @@ def convert_api(response):
         if data['q'] == "USDT":
             coins[data['s']] = {"price":data['c'],"depth":0,"volume":data['qv'],"volume_v":data["v"]}
             coin_name.append(data['s'])
-            print(data['s'],coins[data['s']]["volume_v"])
     return coins, coin_name
